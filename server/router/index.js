6f632865948ee5cbf1a8f44b7e9988f1c47f7ba2
Alexandr Ivanov <alexandr.ivanov.1024@gmail.com>
2022-05-28 15:27:37 +0300

    add user controller

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const Router = require('express').Router;
+const userController = require('../controllers/user_controller');
 
 const router = new Router();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
 
 module.exports = router;
